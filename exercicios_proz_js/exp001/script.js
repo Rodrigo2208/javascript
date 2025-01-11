ac25de2c39b23dfe2b471253304b1ec75658c31f
Rodrigo Durães <rodrigoduraes.nascimento11@gmail.com>
2025-01-11 10:09:08 -0300

    Initial commit

---
+++ exercicios_proz_js/exp001/script.js
@@ -0,0 +1,24 @@
+console.log("ola mundo")
+
+var nome
+console.log(nome)
+nome = "Neymar"
+console.log(nome)
+
+var idade = 2015
+console.log(idade)
+idade = 30 + 20 - 10*5
+console.log(idade)
+
+function mostrar(){
+    console.log("ola mundo")
+}
+
+mostrar()
+
+function soma(n1, n2){
+    return n1 + n2
+}
+console.log(soma(3,7))
+
+
